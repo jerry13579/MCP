93d27bdf7b9ef7094ec803387716fd7de21c2c95
Jerry Cheng <jerrycheng03@gmail.com>
2025-05-20 22:55:52 -0700

    added client

--- agent.py
+++ agent.py
@@ -5,8 +5,8 @@ from mcp import StdioServerParameters
 
 # Specify Ollama LLM via LiteLLM
 model = LiteLLMModel(
-        model_id="ollama_chat/qwen2.5:14b",
-        num_ctx=8192) 
+        model_id="ollama_chat/llama3.2:latest",
+        num_ctx=8192)
 
 # Outline STDIO stuff to get to MCP Tools
 server_parameters = StdioServerParameters(
@@ -18,5 +18,5 @@ server_parameters = StdioServerParameters(
 # Run the agent using the MCP tools 
 with ToolCollection.from_mcp(server_parameters, trust_remote_code=True) as tool_collection:
     agent = ToolCallingAgent(tools=[*tool_collection.tools], model=model)
-    agent.run("What was EBITDA for IBM?")
+    agent.run("Should I buy or sell TSLA right now based on the last 3 months of performance compared to overall market?")
     
--- server.py
+++ server.py
@@ -1,18 +1,26 @@
 from mcp.server.fastmcp import FastMCP
 from stockdex import Ticker
+import json
 
-
-# Create server 
 mcp = FastMCP("stockserver")
 
-# Build server function
+@mcp.prompt()
+def stock_trade_advice(stock_data:str) -> str:
+    """Prompt template for any stock trade advice inquiry"""
+    return f"""You are a helpful financial assistant designed to recommend stock trades.
+               Using the information below, recommend a stock trade based on the user's inquiry with a detailed explanation. Also include a hilariously funny joke about the stock performance
+                Data {stock_data}"""
+                
+
 @mcp.tool()
 def current_stock_price(stock_ticker: str) -> str:
     """This tool returns the last known price for a given stock ticker.
     Args:
         stock_ticker: an alphanumeric stock ticker 
+        Example payload: "TSLA"
     Returns:
-        str:"Ticker: Last Price" 
+        str:"Current price for Ticker: Last Price" 
+        Example response: "Current price for TSLA: 1000.0"
         """
     stock_ticker = stock_ticker.strip().upper()
     ticker = Ticker(ticker=stock_ticker)
@@ -20,23 +28,30 @@ def current_stock_price(stock_ticker: str) -> str:
     return f"Current price for {stock_ticker}: {last_price}"
 
 @mcp.tool()
-def stock_price_3mo(stock_ticker: str) -> str:
-    """This tool returns the past 3 months of stock price data for a given stock ticker.
+def stock_price_performance(stock_ticker: str, time_range: str) -> str:
+    """This tool returns the past period of daily percent changes for a given stock ticker.
     Args:
-        stock_ticker: an alphanumeric stock ticker 
+        stock_ticker: a stock ticker symbol Yahoo Finance recognizes
+        range: a string of '1d', '5d', '1mo', '3mo', '6mo', '1y', '5y'
+        Example payload: "TSLA", "3mo"
     Returns:
-        str:"Ticker: daily % change" 
+        str: JSON string of daily percent changes
+        Example response: {
+            "ticker": stock_ticker,
+            "daily_percent_changes": ticker_change,
+        }
         """
     stock_ticker = stock_ticker.strip().upper()
-    def calculate_change(ticker):
-        ticker = Ticker(ticker=ticker)
-        data = ticker.yahoo_api_price(range='3mo', dataGranularity='1d')
-        return ((data['close'] - data['open']) / data['open'] * 100)
-    ticker_change = calculate_change(stock_ticker).round(2)
-    ticker_vs_index = (ticker_change - calculate_change('^GSPC')).round(2)
-    return f"Daily % change for {stock_ticker}: {ticker_change.tolist()}\nDaily % change compared to S&P 500: {ticker_vs_index.tolist()}"
+    ticker = Ticker(ticker=stock_ticker)
+    data = ticker.yahoo_api_price(range=time_range, dataGranularity='1d')
+    ticker_change = ((data['close'] - data['open']) / data['open'] * 100).round(2).tolist()
+    
+    return {
+        "ticker": stock_ticker,
+        "daily_percent_changes": ticker_change,
+    }
 
 
-# Kick off server if file is run 
+
 if __name__ == "__main__":
     mcp.run(transport="stdio")
