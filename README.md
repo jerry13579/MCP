124e9fcfabba6f4b37cccbc51edc89da96e38005
Jerry Cheng <jerrycheng03@gmail.com>
2025-06-06 19:14:43 -0700

    trying to get it to work with langchain

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 MCP stock advisor to easily do research on stock performance
 Provides stock performance as well as Reddit sentiment analysis (not implemented yet) for LLM
-Also I need to add mechanism to analyze how well the LLM gives advice.
+Also I need to add mechanism to analyze how well the LLM gives advice
--- server.py
+++ server.py
@@ -1,16 +1,8 @@
 from mcp.server.fastmcp import FastMCP
-from stockdex import Ticker
-import json
+import yfinance as yf
 
 mcp = FastMCP("stockserver")
-
-@mcp.prompt()
-def stock_trade_advice(stock_data:str) -> str:
-    """Prompt template for any stock trade advice inquiry"""
-    return f"""You are a helpful financial assistant designed to recommend stock trades.
-               Using the information below, recommend a stock trade based on the user's inquiry with a detailed explanation. Also include a hilariously funny joke about the stock performance
-                Data {stock_data}"""
-                
+   
 
 @mcp.tool()
 def current_stock_price(stock_ticker: str) -> str:
@@ -23,10 +15,13 @@ def current_stock_price(stock_ticker: str) -> str:
         Example response: "Current price for TSLA: 1000.0"
         """
     stock_ticker = stock_ticker.strip().upper()
-    ticker = Ticker(ticker=stock_ticker)
-    last_price = ticker.yahoo_api_price(range='1h', dataGranularity='1h').sort_values('timestamp', ascending=False).iloc[0]['close'].round(2)
-    return f"Current price for {stock_ticker}: {last_price}"
-
+    try:
+        ticker = yf.Ticker(stock_ticker)
+        last_price = ticker.info['regularMarketPrice']
+        return f"Current price for {stock_ticker}: {last_price}"
+    except Exception as e:
+        return f"Error fetching data for {stock_ticker}: {e}"
+'''
 @mcp.tool()
 def stock_price_performance(stock_ticker: str, time_range: str) -> str:
     """This tool returns the past period of daily percent changes for a given stock ticker.
@@ -50,6 +45,20 @@ def stock_price_performance(stock_ticker: str, time_range: str) -> str:
         "ticker": stock_ticker,
         "daily_percent_changes": ticker_change,
     }
+
+
+'''
+
+@mcp.tool()
+def sentiment_analysis(stock_ticker: str) -> str:
+    """This tool returns the sentiment analysis for a given stock ticker.
+    Args:
+        stock_ticker: a stock ticker symbol Yahoo Finance recognizes
+        Example payload: "TSLA"
+    Returns:
+        str: sentiment 
+        """
+    return f"The sentiment for {stock_ticker} is very positive!"
 
 
 
